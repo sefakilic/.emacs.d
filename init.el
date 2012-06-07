87be2625d389fd4e8e6cfe711532b0fef1bfc5ae
Sefa Kilic <sefakilic@gmail.com>
2012-06-07 11:06:31 -0400

    basic settings and uniquify buffer names

--- init.el
+++ init.el
@@ -1,1 +1,20 @@
+;; Set font
 (set-default-font "monospace 10")
+;; Show line-number in the mode line
+(line-number-mode 1)
+;; Show column-number in the mode line
+(column-number-mode 1)
+
+;; ===== Turn on Auto Fill mode automatically in all modes =====
+;; Auto-fill-mode the the automatic wrapping of lines and insertion of
+;; newlines when the cursor goes over the column limit.
+;; This should actually turn on auto-fill-mode by default in all major
+;; modes. The other way to do this is to turn on the fill for specific modes
+;; via hooks.
+(setq-default fill-column 80)
+(setq auto-fill-mode 1)
+
+;; make buffer names unique
+(require 'uniquify)
+(set 'uniquify-buffer-name-style 'forward) 
+
