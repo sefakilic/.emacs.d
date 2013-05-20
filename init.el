3f98e5f48d127c3e884e153ebf5cbbd876aca206
Sefa Kilic <sefakilic@gmail.com>
2013-05-20 19:35:00 -0400

    copy/paste from/to emacs

--- init.el
+++ init.el
@@ -1,16 +1,17 @@
-;; Hi emacs! Let me introduce myself.
+;; Hi emacs! Let me introduce myself. 
 (setq user-full-name "Sefa Kilic")
 (setq user-mail-address "sefakilic@gmail.com")
 
 (add-to-list 'load-path "~/.emacs.d/")
 
 (set-default-font "monaco-10")       ;; Set font
+;;(set-default-font "monospace-10")
 (line-number-mode 1)                  ;; Show line-number in the mode line
 (column-number-mode 1)                ;; Show column-number in the mode line
 (scroll-bar-mode -1)                  ;; use scrollbar
 (tool-bar-mode -1)                    ;; display toolbar
-(turn-on-auto-fill)                   ;; auto-fill mode
 
+(setq-default fill-column 85)
 
 ;; Turn on Auto Fill mode automatically in all modes.
 ;; Auto-fill-mode the the automatic wrapping of lines and insertion of
@@ -18,12 +19,24 @@
 ;; This should actually turn on auto-fill-mode by default in all major
 ;; modes. The other way to do this is to turn on the fill for specific modes
 ;; via hooks.
-(setq-default fill-column 85)
 (setq auto-fill-mode 1)
+
+;; cut-copy-paste to/from emacs
+(setq x-select-enable-clipboard t)
+
+;; Place all backup and autosave files into a directory
+(setq backup-by-copying t                             ; don't clobber symlinks
+      backup-directory-alist '(("." . "~/.saves"))    ; don't litter my fs tree
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)                              ; use versioned backups
+
 
 ;; autopair
 (require 'autopair)
 (autopair-global-mode) ;; enable autopair in all buffers 
+
 ;; show paren mode
 (show-paren-mode 1)
 
@@ -35,10 +48,8 @@
 ;; Dired copy folders recursively without confirmation
 (setq dired-recursive-copies 'always)
 (setq dired-recursive-deletes 'always)
-
 ;; sort directories first
 (setq dired-listing-switches "-alh --group-directories-first")
-
 ;; dired omit files
 (require 'dired-x)
 (setq dired-omit-mode 1)
@@ -61,8 +72,11 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(mediawiki-site-alist (quote (("erilllab" "http://erilllab.biosci.umbc.edu/wiki/" "sefa1" "" "Main Page"))))
- '(mediawiki-site-default "erilllab"))
+ '(mediawiki-site-default "erilllab")
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
