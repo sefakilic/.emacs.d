7ab180ad6c303a910e9d482241503ea49185e759
Sefa Kilic <sefakilic@gmail.com>
2015-02-19 22:29:48 -0500

    added reftex

--- init.el
+++ init.el
@@ -103,7 +103,7 @@
 
 ;; color-theme
 ;; for color-theme package install emacs-goodies
-(require 'zenburn-theme)
+;(require 'zenburn-theme)               
 
 ;; Kill all buffers, except the current one
 (defun kill-other-buffers ()
@@ -219,6 +219,8 @@
 ;; latex
 (require 'tex)
 (TeX-global-PDF-mode t)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
 
 
 ;; spell checking
